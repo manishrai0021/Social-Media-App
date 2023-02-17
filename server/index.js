4a3fc6d84f4e48b36cec9b423789e1f8955885ee
manishrai0021 <94151781+manishrai0021@users.noreply.github.com>
2023-02-17 23:04:15 +0545

    Data 📅

--- server/index.js
+++ server/index.js
@@ -15,6 +15,9 @@ import postRoutes from './routes/post.js';
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // Configuration
 const __filename = fileURLToPath(import.meta.url);
@@ -58,5 +61,9 @@ mongoose.connect(process.env.MONGO_URL,{
 })
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 })
 .catch((error) => console.log(`${error} did not connect`));
