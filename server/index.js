cbbd257e1fff62e52f625fe87b297908240e54e1
manishrai0021 <94151781+manishrai0021@users.noreply.github.com>
2023-02-17 22:40:39 +0545

    Posts 🚩

--- server/index.js
+++ server/index.js
@@ -9,9 +9,12 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import { register } from "./controllers/auth.js";
 import authRoutes from './routes/auth.js';
-// import userRoutes from './routes/users.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/post.js';
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // Configuration
 const __filename = fileURLToPath(import.meta.url);
@@ -40,10 +43,12 @@ const upload = multer({ storage });
 
 // Route with Files
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 //Routes
 app.use("/auth", authRoutes);
-// app.use("/users", userRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // Mongoose Setup
 const PORT = process.env.PORT || 6001;
